b1509933bdccb7eebdcdc85adfca695457e3b1ff
aaronlam <aaronlam2k@gmail.com>
2022-09-29 14:01:05 -0700

    Update Login Page UI

--- pages/login.js
+++ pages/login.js
@@ -6,14 +6,16 @@ function login({ providers }) {
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
       <img
         className="w-1/3 mb-8"
-        src="https://www.scdn.co/i/_global/open-graph-default.png"
+        src="https://cdn2.downdetector.com/static/uploads/logo/Spotify_Logo_RGB_Green.png"
       />
       {Object.values(providers).map((provider) => (
         <div key={provider.id}>
-          <button className="bg-[#18D860] text-white p-5 rounded-lg" 
-            onClick={() => signIn(provider.id, {callbackUrl: "/"})}
+          <button
+            className="bg-[#18D860] text-black p-4 rounded-full hover:bg-[#0a9f43] hover:scale-[1.02]
+          transition transform duration-150 ease-in-out"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
           >
-          Login with {provider.name}
+            Login with {provider.name}
           </button>
         </div>
       ))}

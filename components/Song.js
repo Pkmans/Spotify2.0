acc556f0e73ce9978a689e4323fa5a2b847cbd9a
Aaron Lam <aaronlam2k@gmail.com>
2022-10-05 12:48:52 -0700

    Green text and Audio bar animation for current song

--- components/Song.js
+++ components/Song.js
@@ -1,43 +1,66 @@
 import React from "react";
 import { useRecoilState } from "recoil";
+import Lottie from "lottie-react";
+
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSpotify from "../hooks/useSpotify";
-
 import { millisToMinutesAndSeconds } from "../lib/timeConverter";
+import audioBarAnimation from "../lib/audioBarAnimation.json";
 
 function Song({ playlist, track, order }) {
   const spotifyApi = useSpotify();
-  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
-  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState); 
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
 
   // console.log('playlist:', playlist);
 
-  function playSong(){
+  function playSong() {
     console.log(track);
     setCurrentTrackId(track.track.id);
     setIsPlaying(true);
     spotifyApi.play({
-        context_uri: playlist.uri,
-        offset: {"uri": track.track.uri}
-    })
+      context_uri: playlist.uri,
+      offset: { uri: track.track.uri },
+    });
   }
 
   return (
-    <div className="songs grid grid-cols-2 text-gray-400 px-5 py-1.5 hover:bg-gray-800 rounded-md cursor-pointer"
-        onClick={playSong}
+    <div
+      className="songs grid grid-cols-2 text-gray-400 px-5 py-1.5 hover:bg-gray-800 rounded-md cursor-pointer"
+      onClick={playSong}
     >
       <div className="flex items-center space-x-4">
-        <p>{order + 1}</p>
+        {currentTrackId === track.track.id ? (
+          <Lottie
+            animationData={audioBarAnimation}
+            style={{ width: 30}}
+            loop={true}
+          />
+        ) : (
+          <p>{order + 1}</p>
+        )}
+
         <img className="w-10 h-10" src={track.track.album.images?.[0]?.url} />
         <div className="w-44 md:w-58 lg:w-64">
-          <p className="text-white truncate">{track.track.name}</p>
+          <p
+            className={
+              currentTrackId === track.track.id
+                ? "text-green-500 truncate"
+                : "text-white truncate"
+            }
+          >
+            {track.track.name}
+          </p>
           <p className="truncate ">{track.track.artists[0].name}</p>
         </div>
       </div>
 
       <div className="flex items-center justify-between ml-auto md:ml-0">
         <p className="hidden md:inline truncate">{track.track.album.name}</p>
-        <p className="pl-6">{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
+        <p className="pl-6">
+          {millisToMinutesAndSeconds(track.track.duration_ms)}
+        </p>
       </div>
     </div>
   );
